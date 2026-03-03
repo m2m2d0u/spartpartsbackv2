78b8e80dba7f49f92c1b98781aead5e109c90df2
mamadou-abass-diallo-sre <m.abassdiallo@gmail.con>
2026-03-03 10:33:31 +0000

    Initialize Maven Spring Boot project with essential configurations and dependencies: - Add IntelliJ and Maven-specific `.gitignore` rules. - Include Maven Wrapper scripts (`mvnw`, `mvnw.cmd`) and configuration. - Configure `application.yaml` with a default application name. - Set up

---
+++ src/main/java/sn/symmetry/spareparts/SparepartsApplication.java
@@ -0,0 +1,13 @@
+package sn.symmetry.spareparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparepartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparepartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/symmetry/spareparts/SparepartsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.symmetry.spareparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparepartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
